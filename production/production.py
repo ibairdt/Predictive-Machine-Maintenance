f354dcec61da164445dfe34880891aaf5d953654
ibairdt <ibai.valente@rdtingenieros.com>
2024-11-13 16:17:55 +0100

    añadida lógica captura de excepciones

--- production/production.py
+++ production/production.py
@@ -7,30 +7,38 @@ script_dir = Path(__file__).parent
 
 # incoming dataset import
 csv_path = script_dir.parent / 'data' / 'incoming' / 'incoming_data.csv'
-if csv_path.exists():
+try:
     df_incoming = pd.read_csv(csv_path, sep=";")
     print("CSV loaded")
-else:
+
+except FileNotFoundError:
     print(f"CSV {csv_path} does not exist")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
 
 # model import
 model_path = script_dir.parent / 'models' / 'rf_class.pkl'
-if model_path.exists():
+try:
     with open(model_path, "rb") as model_file:
         model = pickle.load(model_file)
-    print("Model loaded")
-else:
+        print("Model loaded")
+
+except FileNotFoundError:
     print(f"Model {model_path} does not exist")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
 
 # scaler import
 scaler_path = script_dir.parent / 'scalers' / 'std_scaler.pkl'
-if scaler_path.exists():
+try:
     with open(scaler_path, "rb") as scaler_file:
         scaler = pickle.load(scaler_file)
-    print("Scaler loaded")
-else:
-    print(f"Scaler {scaler_path} does not exist")
+        print("Scaler loaded")
 
+except FileNotFoundError:
+    print(f"Scaler {scaler_path} does not exist")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
 
 # data preprocessing
 df_incoming = df_incoming.drop(columns=["UDI", "Product ID", "Failure Type", "Air temperature [K]", "Rotational speed [rpm]"])
